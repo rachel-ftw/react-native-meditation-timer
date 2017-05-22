0f647c49cb76605dd5032953a240d24d07029a20
Rachel Ralston <rachelralston@gmail.com>
2017-05-22 11:09:59 -0700

    adding updates

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
-import Expo from 'expo';
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  TextInput
-} from 'react-native';
-// import { BigButton } from './components/bigButton';
-// import Timer from './components/Timer'
+import React, { Component } from 'react';
 import { combineReducers, createStore } from 'redux'
 import { connect, Provider } from 'react-redux'
-// import { addTimer } from './actions'
-// import { ADD_TIMER } from './constants/ActionTypes'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
-// let nextTimerId = 0
-//
-// const addTimer = (time) => ({ //acton
-//   type: 'ADD_TIMER',
-//   id: nextTodoId ++,
-//   meditation_time: time
-// })
-
-// const AddTodoInput = connect((state) => ({
-//   value: state.meditation_time
-// }), (dispatch) => ({
-//   onChangeText: text => {
-//     dispatch(addTimer)
-//   }
-// }))(TextInput)
 
 const userDefaults = {name: "Rachel", age: 31}
 const userReducer = (state=userDefaults, action) => {
@@ -68,35 +49,47 @@ const reducers = combineReducers({
   timers: timersReducer
 })
 
+const mapStateToProps = state => ({user: state.user, timers: state.timers})
+
+const matchDispatchToProps = dispatch => ({
+  changeName: name => dispatch({type: "CHANGE_NAME", payload: name}),
+  changeAge: age => dispatch({type: "CHANGE_AGE", payload: age}),
+  addTimer: (id, meditation_time) => dispatch({type: "ADD_TIMER", payload: {id, meditation_time}}),
+  deleteTimer: id => dispatch({type: "DELETE_TIMER", payload: {id}}),
+  editTimer: (id, meditation_time) => dispatch({type: "EDIT_TIMER", payload: {id, meditation_time}})
+})
+
+
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+  constructor() {
+    super()
     this.store = createStore(reducers)
   }
   render() {
     this.store.subscribe(() => {
       console.log('in the render, subscrition: ', this.store.getState())
     })
-    this.store.dispatch({type: "CHANGE_NAME", payload: "Kato"})
-    this.store.dispatch({type: "CHANGE_AGE", payload: 35})
-    this.store.dispatch({type: "ADD_TIMER", payload: {id: 2, meditation_time: 15}})
-    this.store.dispatch({type: "ADD_TIMER", payload: {id: 3, meditation_time: 327}})
-    this.store.dispatch({type: "DELETE_TIMER", payload: {id: 3}})
-    this.store.dispatch({type: "EDIT_TIMER", payload: {id: 2, meditation_time: 2}})
-    console.log('!!!!!!!!!!', this.store)
+    // this.store.dispatch({type: "CHANGE_NAME", payload: "Kato"})
+    // this.store.dispatch({type: "CHANGE_AGE", payload: 35})
+    // this.store.dispatch({type: "ADD_TIMER", payload: {id: 2, meditation_time: 15}})
+    // this.store.dispatch({type: "ADD_TIMER", payload: {id: 3, meditation_time: 327}})
+    // this.store.dispatch({type: "DELETE_TIMER", payload: {id: 3}})
+    // this.store.dispatch({type: "EDIT_TIMER", payload: {id: 2, meditation_time: 2}})
+    console.log('!!!!!!!!!!', this.props)
     return (
       <Provider store={this.store}>
         <View style={styles.container}>
           <TextInput placeholder="minutes to meditate" />
           <TouchableHighlight>
-            <Text>Add Timer</Text>
+            <Text>hi </Text>
           </TouchableHighlight>
         </View>
       </Provider>
     );
   }
 }
+connect(mapStateToProps, matchDispatchToProps)(App)
+
 
 
 const styles = StyleSheet.create({
@@ -108,4 +101,4 @@ const styles = StyleSheet.create({
   }
 });
 
-Expo.registerRootComponent(App);
+AppRegistry.registerComponent('App', () => App);
